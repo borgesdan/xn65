c7118d7abcb79af46dd87c455bc4f13cde0cb61d
Danilo <borges_santos89@hotmail.com>
2024-04-27 15:31:23 -0300

    Adiciona ServiceContainer

---
+++ framework/game/servicecontainer.hpp
@@ -0,0 +1,14 @@
+#ifndef XNA_GAME_SERVICECONTAINER_HPP
+#define XNA_GAME_SERVICECONTAINER_HPP
+
+#include "../default.hpp"
+
+namespace xna {
+	class IServiceProvider {
+	};
+
+	class GameServiceContainer {
+	};
+}
+
+#endif
