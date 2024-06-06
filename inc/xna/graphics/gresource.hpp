da0a300999233d50c8c8da217b0a3f12f15e7ed1
Danilo <borges_santos89@hotmail.com>
2024-06-06 14:47:39 -0300

    Implementações em GraphicsResource

--- inc/xna/graphics/gresource.hpp
+++ inc/xna/graphics/gresource.hpp
@@ -4,20 +4,17 @@
 #include "../default.hpp"
 
 namespace xna {
+	//Queries and prepares resources. 
 	class GraphicsResource {
 	public:
-		GraphicsResource(sptr<GraphicsDevice> const& device) : m_device(device){}
+		GraphicsResource(sptr<GraphicsDevice> const& device);
 
 		virtual ~GraphicsResource(){}
 
-		virtual bool Bind(sptr<GraphicsDevice> const& device) {
-			if (!device || device == m_device)
-				return false;
+		virtual bool Bind(sptr<GraphicsDevice> const& device);
 
-			m_device = device;
-
-			return true;
-		}
+		//Gets the GraphicsDevice associated with this GraphicsResource.
+		sptr<xna::GraphicsDevice> Device() const;
 
 	protected:
 		sptr<GraphicsDevice> m_device = nullptr;
