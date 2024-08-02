a60e0f366dd277fde173688dd6f00471c6d7c864
Danilo <borges_santos89@hotmail.com>
2024-08-02 11:31:54 -0300

    Comentários em GameTime

--- inc/xna/game/time.hpp
+++ inc/xna/game/time.hpp
@@ -5,6 +5,7 @@
 #include "../csharp/timespan.hpp"
 
 namespace xna {
+	//Snapshot of the game timing state expressed in values that can be used by variable-step (real time) or fixed-step (game time) games. 
 	class GameTime {	
 	public:
 		constexpr GameTime() = default;
@@ -14,8 +15,11 @@ namespace xna {
 			IsRunningSlowly(isRunningSlowly), 
 			TotalGameTime(totalGameTime) { }		
 
+		//The amount of elapsed game time since the last update.
 		TimeSpan ElapsedGameTime{ 0 };
+		//Gets a value indicating that the game loop is taking longer than its TargetElapsedTime. In this case, the game loop can be considered to be running too slowly and should do something to "catch up." 
 		bool IsRunningSlowly{ false };
+		//The amount of game time since the start of the game.
 		TimeSpan TotalGameTime{ 0 };		
 	};
 }
