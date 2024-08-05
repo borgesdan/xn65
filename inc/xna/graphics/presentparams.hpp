275233589343684b785c9c666bbd6fdba39d4c1a
Danilo <borges_santos89@hotmail.com>
2024-08-05 17:38:35 -0300

    Comentários em PresentationParameters

--- inc/xna/graphics/presentparams.hpp
+++ inc/xna/graphics/presentparams.hpp
@@ -4,17 +4,24 @@
 #include "../default.hpp"
 
 namespace xna {
+	//Contains presentation parameters. 
 	struct PresentationParameters {
-		constexpr PresentationParameters() = default;
-
+		//Gets or sets a value indicating the width of the new swap chain's back buffer.
 		Int BackBufferWidth{ 0 };
+		//Gets or sets a value indicating the height of the new swap chain's back buffer.
 		Int BackBufferHeight{ 0 };
+		//Gets or sets the format of the back buffer.
 		SurfaceFormat BackBufferFormat{ SurfaceFormat::Color };
 		SwapEffect PresentationSwapEffect{ SwapEffect::FlipDiscard };
+		//Gets or sets the handle to the device window. 
 		intptr_t DeviceWindowHandle{ 0 };
+		//Gets or sets a value indicating whether the application is in full screen mode.
 		bool IsFullscreen{ false };
+		//Gets or sets a value indicating the number of sample locations during multisampling.
 		Int MultiSampleCount{ 0 };
+		//Gets or sets the maximum rate at which the swap chain's back buffers can be presented to the front buffer.
 		PresentInterval PresentationInterval{ PresentInterval::Default };
+		//Gets or sets the depth stencil data format.
 		DepthFormat DepthStencilFormat{ DepthFormat::None };
 	};
 }
